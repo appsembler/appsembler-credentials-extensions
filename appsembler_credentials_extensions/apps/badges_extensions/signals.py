4247909d02e0e28c5d351cd0cfe227ad5328be0e
Bryan Wilson <bryanlandia@gmail.com>
2018-07-20 13:39:56 -0700

    Badges_extensions signals module for setttings handlers

---
+++ appsembler_credentials_extensions/apps/badges_extensions/signals.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Custom signal handlers for badges."""
+
+from __future__ import absolute_import, unicode_literals
+
+from django.conf import settings
+from django.dispatch.dispatcher import receiver
+
+from xmodule.modulestore.django import SignalHandler, modulestore
+
+from appsembler_credentials_extensions.apps.badges_extensions import app_settings
+
+
+@receiver(SignalHandler.pre_publish)
+def _change_badges_setting_on_pre_publish(sender, course_key, **kwargs):  # pylint: disable=unused-argument
+    store = modulestore()
+    course = store.get_course(course_key)
+    use_badges = settings.FEATURES.get('ENABLE_OPENBADGES', False)
+    if not use_badges or app_settings.DISABLE_COURSE_COMPLETION_BADGES:
+        course.issue_badges = False
+    course.save()
+    try:
+        store.update_item(course, course._edited_by)
+    except AttributeError:
+        store.update_item(course, 0)
