37750e3345721cf9de8070d87a3b0c83f9e7b545
Bryan Wilson <bryanlandia@gmail.com>
2018-08-07 16:45:50 -0700

    use CERTIFICATES_HTML_VIEW not CERTIFICATES_ENABLED (pdf certs) for feature activation helper

--- appsembler_credentials_extensions/apps/course_certs_extensions/helpers.py
+++ appsembler_credentials_extensions/apps/course_certs_extensions/helpers.py
@@ -42,9 +42,9 @@ def cms_import_helper():
 def disable_if_certs_feature_off(func):
     @wraps(func)
     def noop_func(*args, **kwargs):
-        logger.warn('{} disabled since CERTIFICATES_ENABLED is False'.format(func.__name__))
+        logger.warn('{} disabled since CERTIFICATES_HTML_VIEW is False'.format(func.__name__))
 
-    if settings.FEATURES.get('CERTIFICATES_ENABLED', False):
+    if settings.FEATURES.get('CERTIFICATES_HTML_VIEW', False):
         return func
     else:
         noop_func.__name__ = str('noop_func_{}'.format(func.__name__))  # py2.7 needs __name__ as String obj
