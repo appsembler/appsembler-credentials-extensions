62abcc07fa5888bc80a9ba2273b6a5c98870664e
Bryan Wilson <bryanlandia@gmail.com>
2019-04-02 21:38:32 -0700

    Release version to 0.1.2

--- appsembler_credentials_extensions/__init__.py
+++ appsembler_credentials_extensions/__init__.py
@@ -4,4 +4,4 @@ Common package with apps for extensions for Open edX course/program certificates
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
